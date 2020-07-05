c4871bda552367c878cef66997bd531731143fda
Mikhail <m.a.gvozdev@gmail.com>
2020-07-05 16:11:33 +0300

    Edit bullet

--- Курсы Миши/Assets/Scripts/BulletRun.cs
+++ Курсы Миши/Assets/Scripts/BulletRun.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletRun : MonoBehaviour
@@ -9,32 +8,21 @@ public class BulletRun : MonoBehaviour
     [SerializeField] private float _range;
     private float _progress; 
     public event Action<GameObject> Destroyed;
-    private GameObject _target; 
+
+    private IEnumerator Start()
+    {
+        while (_progress < _range)
+        {
+            yield return null;
+            transform.position += transform.forward * (Time.deltaTime * _speed); // тут не могу понять как написать движение вдоль форворда 
+            _progress += Time.deltaTime * _speed;
+        }
+        Destroy(gameObject);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        _target = other.gameObject;
-        Destroy(this.gameObject);
-    }
-
-    private void OnDestroy()
-    {
-        Destroyed?.Invoke(_target);
-    }
-
-    private void Move()
-    {
-        transform.position += transform.forward * (Time.deltaTime * _speed); // тут не могу понять как написать движение вдоль форворда пули
-        _progress += Time.deltaTime * _speed;
-
-        if (_progress >= _range)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    private void Update()
-    {
-        Move();
+        Destroyed?.Invoke(other.gameObject);
+        Destroy(gameObject);
     }
 }

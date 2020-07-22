fc66e9e07763f32841d625d156af44c7cc613b14
reznik <reznikpd@gmail.com>
2020-07-22 21:51:19 +0300

    123

---
+++ New Unity Project/Assets/Scripts/Models/MatrixIndex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    [Serializable]
+    public struct MatrixIndex 
+    {
+        public int X;
+        public int Y;
+
+        public MatrixIndex(int x, int y)
+        {
+            Y = y;
+            X = x;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Models/TileData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Models
+{
+    public struct TileData
+    {
+        public MatrixIndex Index;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return Mebel == null;
+            }
+        }
+
+        public bool IsDirty
+        {
+            get
+            {
+                return Dust != null;
+            }
+        }
+
+        public GameObject Dust;
+        public GameObject Mebel; 
+
+        public GameObject TileModel;
+        
+    }
+}
